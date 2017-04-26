392eb9f6f649b605eded182a6ee8071e8a3db6ab
Mike Krinkin <krinkin.m.u@gmail.com>
2017-04-26 22:17:52 +0300

    week3: update threading example

--- week3/threads/threads/src/main.c
+++ week3/threads/threads/src/main.c
@@ -28,14 +28,44 @@ static int threadx(void *arg)
 {
 	const int id = (intptr_t)arg;
 
-	while (1)
+	while (1) {
+		/**
+		 * You might be surprised what effects may cause
+		 * unsychronized calls to printf from different threads.
+		 * But synchronization is a subject of different week,
+		 * so far we just block interrupts while we printing
+		 * something.
+		 **/
+		const int enabled = local_int_save();
+
 		printf("I'm thread %d\n", id);
+		local_int_restore(enabled);
+	}
 	return 0;
+}
+
+static int thread0(void *unused)
+{
+	(void) unused;
+
+	for (int i = 0; i != 100000000; ++i);
+	printf("Thread0: return 42\n");
+	return 42;
 }
 
 static int init(void *unused)
 {
 	(void) unused;
+
+	struct thread *thread = thread_create(&thread0, 0);
+	int ret;
+
+	thread_start(thread);
+	thread_join(thread, &ret);
+	thread_destroy(thread);
+	printf("Thread0 returned %d\n", ret);
+
+	for (int i = 0; i != 100000000; ++i);
 
 	struct thread *thread1 = thread_create(&threadx, (void *)1);
 	struct thread *thread2 = thread_create(&threadx, (void *)2);
@@ -43,7 +73,12 @@ static int init(void *unused)
 	thread_start(thread1);
 	thread_start(thread2);
 
-	while (1);
+	while (1) {
+		const int enabled = local_int_save();
+
+		printf("I'm thread 0\n");
+		local_int_restore(enabled);
+	}
 	return 0;
 }
 
